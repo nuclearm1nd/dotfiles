202fac95fbac53919ccabfa3036e006953903b53
Volodymyr Savchenko <vovasvch@gmail.com>
2022-07-23 18:48:28 +0300

    add fontconfig

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ ln -sv ~/.dotfiles/.i3 ~
 ln -sv ~/.dotfiles/alacritty ~/.config/
 ln -sv ~/.dotfiles/polybar ~/.config/
 ln -sv ~/.dotfiles/rofi ~/.config/
+ln -sv ~/.dotfiles/fontconfig/fonts.conf ~/.config/fontconfig/fonts.conf
 
